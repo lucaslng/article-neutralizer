05de7f933c1587b47e839895bc8f019a291ccbf9
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-11-08 16:50:15 -0500

    implement save article function

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
+
 export default function Saved() {
+	const [articles, setArticles] = useState([]);
+
+	useEffect(() => {
+		const listener = () => {
+			chrome.storage.local.get(["savedArticles"], (r) => {
+			setArticles(r.savedArticles || []);
+			});
+		};
+		chrome.storage.onChanged.addListener(listener);
+		listener();
+		return () => chrome.storage.onChanged.removeListener(listener);
+		}, []);
+
+
 	return (
 		<div>
-			<h1>Saved</h1>
-			<p>wow</p>
+			<ul>
+				{articles.map((article, index) => (
+					<li key={index}>{article}</li>
+				))};
+			</ul>
 		</div>
 	);
 }
