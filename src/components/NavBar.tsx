e86ef0a1a5535ac0155715f2874afea3eb705384
lucaslng <saculgnuel1@gmail.com>
2025-11-08 02:12:10 -0500

    hi

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,15 @@
 export default function NavBar(props: {page: string, setPage: (page: string) => void}) {
 	return (
-		<div className="fixed bg-slate-900 p-8 bottom-0 w-full h-26">
-			<ul className="flex flex-row gap-8 justify-items-stretch">
+		<div className="fixed bg-slate-900 p-4 bottom-0 w-full h-26">
+			<ul className="flex flex-row gap-8 justify-center">
 				<li>
-					<button className="basis-1/3" onClick={() => props.setPage("neutralize")}>Neutralize</button>
+					<button className="basis-1/3 text-center" onClick={() => props.setPage("neutralize")}>Neutralize</button>
 				</li>
 				<li>
-					<button className="basis-1/3" onClick={() => props.setPage("saved")}>Saved</button>
+					<button className="basis-1/3 text-center" onClick={() => props.setPage("saved")}>Saved</button>
 				</li>
 				<li>
-					<button className="basis-1/3" onClick={() => props.setPage("settings")}>Settings</button>
+					<button className="basis-1/3 text-center" onClick={() => props.setPage("settings")}>Settings</button>
 				</li>
 			</ul>
 		</div>
