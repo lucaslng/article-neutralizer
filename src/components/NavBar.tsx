0ae3f8b6a58c217293aedc0c1b11db491c0b2cfa
lucaslng <saculgnuel1@gmail.com>
2025-11-08 16:05:24 -0500

    extract navbar item

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,22 @@
+function NavBarItem(props: {page: string, label: string, setPage: (page: string) => void}) {
+	return (
+		<li className="grow basis-1/3 text-center">
+			<button onClick={() => props.setPage(props.page)}>
+				<div className="flex flex-col gap-4">
+					<h6>{props.label}</h6>
+				</div>
+			</button>
+		</li>
+	);
+}
+
 export default function NavBar(props: {page: string, setPage: (page: string) => void}) {
 	return (
-		<div className="fixed bg-slate-900 p-4 bottom-0 w-full h-26">
+		<div className="fixed bg-slate-900 py-4 bottom-0 w-full h-26">
 			<ul className="flex flex-row gap-8 justify-center">
-				<li>
-					<button className="basis-1/3 text-center" onClick={() => props.setPage("neutralize")}>Neutralize</button>
-				</li>
-				<li>
-					<button className="basis-1/3 text-center" onClick={() => props.setPage("saved")}>Saved</button>
-				</li>
-				<li>
-					<button className="basis-1/3 text-center" onClick={() => props.setPage("settings")}>Settings</button>
-				</li>
+				<NavBarItem page="neutralize" label="Neutralize" setPage={props.setPage}></NavBarItem>
+				<NavBarItem page="saved" label="Saved" setPage={props.setPage}></NavBarItem>
+				<NavBarItem page="settings" label="Settings" setPage={props.setPage}></NavBarItem>
 			</ul>
 		</div>
 	);
